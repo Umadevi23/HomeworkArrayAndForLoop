245d0d73fea1cc5a44babee74699b37e9831516b
Umadevi23 <uma_gorakati@yahoo.co.in>
2019-03-26 11:35:18 +0000

    MyTimeTables

---
+++ src/love/programming/Timetables.java
@@ -0,0 +1,47 @@
+package love.programming;
+
+public class Timetables {
+    public static void main(String[] args) {
+        int b;
+        //int a = 10;
+        /*
+
+
+        for (int a=1; a<=10 ; a++)
+        {
+
+            for (int i=0; i<=10 ; i++)
+            {
+                b = a*i;
+
+                System.out.println(" "+a+"*"+i+"="+b);
+            }
+        }
+*/
+
+        //
+
+        int[] myNumbers = { 1, 2, 3, 4,5,6,7,8,9,10};
+
+
+
+        for (int c=0; c<=9 ; c++)
+        {
+
+            for (int i=0; i<=10 ; i++)
+            {
+                b = myNumbers[c]*i;
+
+                System.out.println(" "+myNumbers[c]+"*"+i+"="+b);
+            }
+        }
+
+
+
+
+    }
+
+
+
+
+}
